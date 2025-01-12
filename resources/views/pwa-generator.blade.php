929fff8e7307b239352479a589d4cf1f9971ab8d
Eugene van der Merwe <eugene@vander.host>
2025-01-12 19:33:49 +0200

    add much boilerplate

--- resources/views/pwa-generator.blade.php
+++ resources/views/pwa-generator.blade.php
@@ -7,6 +7,214 @@
 </head>
 <body class="p-8">
 <div class="max-w-2xl mx-auto">
+    <h1 class="text-2xl font-bold mb-4">PWA Helpers for Laravel</h1>
+
+    <h2 class="text-xl font-bold mb-4">Filament AdminPanelProvder</h1>
+
+<pre>
+    ->tenant(Tenant::class)
+    ->tenantRegistration(RegisterNewTenant::class)
+    ->tenantProfile(EditMyTenantProfile::class)
+    ->registration()
+    ->passwordReset()
+    ->tenantMiddleware([
+        ApplyTenantScopes::class,
+    ], isPersistent: true)
+    ->sidebarCollapsibleOnDesktop()
+    ->plugin(
+        FilamentLaravelLogPlugin::make()
+            ->authorize(
+                fn () => auth()->user()->role([Role::Administrator])
+            )
+    )
+    ->databaseNotifications()
+    ->renderHook(
+        'panels::auth.login.form.after',
+        fn () => view('auth.socialite.google')
+    )
+    ->profile(EditProfile::class)
+    ->colors([
+        'primary' => '#2f4e6f', // Dark blue
+    ]);
+</pre>
+<br>
+<hr>
+<br>
+    <h2 class="text-xl font-bold mb-4">Tenant Model</h1>
+<pre>
+    class Tenant extends Model
+    {
+        use HasFactory;
+    
+        protected $fillable = [
+            'name',
+        ];
+    
+        public function users(): BelongsToMany
+        {
+            return $this->belongsToMany(User::class);
+        }
+    }
+</pre>
+<br>
+<hr>
+<br>
+    <h2 class="text-xl font-bold mb-4">User Model</h1>
+<pre>
+    class User extends Authenticatable implements FilamentUser, HasTenants
+    {
+        use HasFactory;
+        use Notifiable;    
+        use HasPushSubscriptions;
+    
+        protected $fillable = [
+            'name',
+            'email',
+            'password',
+            'google_id',
+            'active',
+        ];
+    
+        protected $hidden = [
+            'password',
+            'remember_token',
+        ];
+    
+        protected function casts(): array
+        {
+            return [
+                'role' => Role::class,
+                'email_verified_at' => 'datetime',
+                'password' => 'hashed',
+            ];
+        }
+    
+        public function canAccessPanel(Panel $panel): bool
+        {
+            return true;
+        }
+    
+        public function tenants(): BelongsToMany
+        {
+            return $this->belongsToMany(Tenant::class);
+        }
+    
+        public function getTenants(Panel $panel): Collection
+        {
+            return $this->tenants;
+        }
+    
+        public function canAccessTenant(Model $tenant): bool
+        {
+            return $this->tenants()->whereKey($tenant)->exists();
+        }
+    
+        // Add relationships...
+    }
+</pre>
+<br>
+<hr>
+<br>
+    <h2 class="text-xl font-bold mb-4">Apply Tenant Scopes</h1>
+<pre>
+    namespace App\Http\Middleware;
+    
+    use App\Models\Setting;
+    use Closure;
+    use Filament\Facades\Filament;
+    use Illuminate\Database\Eloquent\Builder;
+    use Illuminate\Http\Request;
+    use Symfony\Component\HttpFoundation\Response;
+    
+    class ApplyTenantScopes
+    {
+        /**
+         * Handle an incoming request.
+         *
+         * @param  Closure(Request): (Response)  $next
+         */
+        public function handle(Request $request, Closure $next): Response
+        {
+            Setting::addGlobalScope(
+                fn (Builder $query) => $query->whereBelongsTo(Filament::getTenant()),
+            );
+
+            // ...
+    
+            return $next($request);
+        }
+    }
+</pre>
+<br>
+<hr>
+<br>
+    <h2 class="text-xl font-bold mb-4">RegisterNewTenant::class</h1>
+<pre>
+    namespace App\Filament\Pages\Tenancy;
+
+    use App\Models\Tenant;
+    use App\Models\User;
+    use Filament\Forms\Components\TextInput;
+    use Filament\Forms\Form;
+    use Filament\Pages\Tenancy\RegisterTenant;
+    
+    class RegisterNewTenant extends RegisterTenant
+    {
+        public static function getLabel(): string
+        {
+            return 'Register';
+        }
+    
+        public function form(Form $form): Form
+        {
+            return $form
+                ->schema([
+                    TextInput::make('name')
+                        ->helperText('The name of your workspace.')
+                        ->columnSpanFull(),
+                ]);
+        }
+    
+        protected function handleRegistration(array $data): Tenant
+        {
+            $tenant = Tenant::create($data);
+    
+            $tenant->users()->attach(auth()->user());
+        
+            return $tenant;
+        }
+    }
+    
+</pre>
+<br>
+<hr>
+<br>
+    <h2 class="text-xl font-bold mb-4">EditExistingTenantProfile::class</h1>
+<pre>
+    namespace App\Filament\Pages\Tenancy;
+
+    use Filament\Forms\Components\TextInput;
+    use Filament\Forms\Form;
+    use Filament\Pages\Tenancy\EditTenantProfile;
+    
+    class EditExistingTenantProfile extends EditTenantProfile
+    {
+        public static function getLabel(): string
+        {
+            return 'Workspace';
+        }
+    
+        public function form(Form $form): Form
+        {
+            return $form
+                ->schema([
+                    TextInput::make('name'),
+                ]);
+        }
+    }
+    
+</pre>
+
     <h1 class="text-2xl font-bold mb-4">PWA Asset Generator</h1>
 
     <textarea id="svgInput" class="w-full h-32 p-2 mb-4 border rounded" placeholder="Paste your SVG code here..."></textarea>
